fb1c6abe8305b58d698c3ddb6f9f75e229abf6db
Eric Workman <eworkman@bitfreighter.com>
2025-02-26 13:50:54 -0500

    wip

--- lib/wallaby/chrome.ex
+++ lib/wallaby/chrome.ex
@@ -367,7 +367,6 @@ defmodule Wallaby.Chrome do
     |> Keyword.get_lazy(:capabilities, &default_capabilities/0)
     |> put_headless_config(opts)
     |> put_binary_config(opts)
-    |> IO.inspect(label: "capabilities")
   end
 
   @spec wait_until_ready!(timeout) :: :ok | no_return
